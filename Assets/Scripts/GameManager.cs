ff86737c8142327cc9987584d3e6910ed6f9a472
Michael Rinaldi <mikerinaldi1@yahoo.com>
2016-10-08 10:48:43 -0400

    OrderCreator finished

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,10 +7,14 @@ public class GameManager : MonoBehaviour {
 
     //General list of ingredients, this will be the ordering of the ingredients on the counter
     //Can be changed to fit design
+    public static List<Ingredient> meats = new List<Ingredient>() { Ingredient.tofu, Ingredient.tuna, Ingredient.crab };
+    public static List<Ingredient> condiments = new List<Ingredient>() { Ingredient.ginger, Ingredient.wasabi, Ingredient.soySauce };
+    public static List<Ingredient> rice = new List<Ingredient>() { Ingredient.whiteRice, Ingredient.brownRice };
     public static List<Ingredient> indexToIngredient = new List<Ingredient>() {
         Ingredient.tofu, Ingredient.tuna, Ingredient.crab,
         Ingredient.ginger, Ingredient.wasabi, Ingredient.soySauce,
         Ingredient.whiteRice, Ingredient.brownRice };
+
 
     //For easy, medium, hard, and unlimited
     public List<int> highScores = new List<int>() { 0, 0, 0, 0 };
@@ -33,4 +37,11 @@ public class GameManager : MonoBehaviour {
         return currentOrder.addIngredient(i);
     }
 	
+    public static bool isRice(Ingredient i) { return rice.Contains(i); }
+    public static bool isMeat(Ingredient i) { return meats.Contains(i); }
+    public static bool isCondiment(Ingredient i) { return condiments.Contains(i); }
+    public static Ingredient getRandomRice() { return rice[Random.Range(0, rice.Count)]; }
+    public static Ingredient getRandomMeat() { return meats[Random.Range(0, meats.Count)]; }
+    public static Ingredient getRandomCondiment() { return condiments[Random.Range(0, condiments.Count)]; }
+
 }
--- Assets/Scripts/OrderCreator.cs
+++ Assets/Scripts/OrderCreator.cs
@@ -14,17 +14,40 @@ public class OrderCreator {
         //Add a random ingredient for each numIngredient
         int possibleIngredients = GameManager.indexToIngredient.Count;
         int lastPickedIngredient = -1;
+        bool containsRice = false;
+        bool containsMeat = false;
+
         for (int i = 0; i < numIngredients; i++) {
             int nextIngredient = Random.Range(0, possibleIngredients);
             if (nextIngredient != lastPickedIngredient) {
                 //this makes it less likely that two of the same ingredients would be placed in a row
                 nextIngredient = Random.Range(0, possibleIngredients);
             }
-            ingredients.Add(GameManager.indexToIngredient[nextIngredient]);
+            Ingredient ingredient = GameManager.indexToIngredient[nextIngredient];
+            ingredients.Add(ingredient);
+            if (GameManager.isRice(ingredient)) {
+                containsRice = true;
+            }
+            if (GameManager.isMeat(ingredient)) {
+                containsMeat = true;
+            }
         }
 
         //TODO - if doesnt contain a rice, add rice (dont overwrite any meat added unless necessary)
         //TODO - if doesnt contain a meat, add meat (dont overwrite any rice added unless necessary)
+        //Only manually override on the last ingredient
+        if (!containsMeat || !containsRice) {
+            for (int i = 0; i < ingredients.Count; i++) {
+                if (!containsMeat && (!GameManager.isRice(ingredients[i]) || i == ingredients.Count - 1)) {
+                    ingredients[i] = GameManager.getRandomMeat();
+                    containsMeat = true;
+                }
+                if (!containsRice && (!GameManager.isMeat(ingredients[i]) || i == ingredients.Count - 1)) {
+                    ingredients[i] = GameManager.getRandomRice();
+                    containsRice = true;
+                }
+            }
+        }
 
         return new Order(ingredients);
     }
