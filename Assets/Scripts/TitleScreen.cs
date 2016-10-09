5fad0faabf36c30472c66ac0c43c3bf354386628
barrettand <barrett.and@husky.neu.edu>
2016-10-09 10:15:33 -0400

    Title Screen
    Implemented basic title screen with space to transition to next level

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour {
+
+    SoundHandler sh;
+	// Use this for initialization
+	void Start () {
+        sh = FindObjectOfType<SoundHandler>();
+        sh.PlaySound("level_loop_1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            SceneManager.LoadScene("TestLevel");
+        }
+	}
+}
